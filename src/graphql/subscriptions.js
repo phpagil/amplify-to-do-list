a5ef7a3566090a2359b0ded4d34ae3ea576ffb52
Azlina Yeo <azlinayeo@Azlinas-MBP.lan>
2020-07-07 16:38:14 +0100

    set up authentification

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      item
+      priority
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      item
+      priority
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      item
+      priority
+      createdAt
+      updatedAt
+    }
+  }
+`;
